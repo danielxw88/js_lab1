ca30c12a082fc3c8e113a864f5848ad451f8ce0f
danielxw88 <danielxiawang@gmail.com>
2024-09-19 19:25:55 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+//Declare and Use Variables
+
+let studentName="Daniel";
+let studentAge=35;
+let Enrolled=true;
+
+console.log(studentName);
+console.log(studentAge);
+console.log(Enrolled);
+
+//Demonstrate Data Types
+let string_type="string";
+let number_type=3.14;
+let boolean_type=false;
+let array_type=["Apple","banana"];
+let object_type={
+    name:'Daniel',
+    age:35,
+    enroll:true}
+
+console.log(string_type);
+console.log(number_type);
+console.log(boolean_type);
+console.log(array_type);
+console.log(object_type)
+
+
+//Use Basic Operators
+//Arithmetic Operators
+let num1=88.33;
+let num2=19900;
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
+
+//Logical Operators
+let bool1=true;
+let bool2=false;
+console.log(bool1&&bool2);
+console.log(bool1||bool2);
+console.log(!bool1);
